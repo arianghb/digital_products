54398acacf4b96039498c063852b2d66431c87cf
arian <arighbad@gmail.com>
2023-11-04 21:14:32 +0330

    Products models migrated.

--- products/models.py
+++ products/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 class Category(models.Model):
     parent = models.ForeignKey('self', verbose_name=_('parent'), blank=True, null=True, on_delete=models.CASCADE)
@@ -21,7 +21,7 @@ class Product(models.Model):
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True)    
     avatar = models.ImageField(_('avatar'), blank=True, upload_to='products/')
-    categories = models.ManyToManyField('Cateogry', verbose_name=_('categories'), blank=True, null=True, on_delete=models.CASCADE)
+    categories = models.ManyToManyField('Category', verbose_name=_('categories'), blank=True)
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
     updated_time = models.DateTimeField(_('updated time'), auto_now=True)
@@ -35,7 +35,7 @@ class Product(models.Model):
 class File(models.Model):
     title = models.CharField(_('title'), max_length=50)
     description = models.TextField(_('description'), blank=True)    
-    file = models.FileField(_('file'), blank=True, upload_to='files/%Y/%m/%d')
+    file = models.FileField(_('file'), blank=True, upload_to='files/%Y/%m/%d/')
     product = models.ForeignKey('Product', verbose_name=_('product'), blank=True, null=True, on_delete=models.CASCADE)
     is_enable = models.BooleanField(_('is enable'), default=True)
     created_time = models.DateTimeField(_('created time'), auto_now_add=True)
